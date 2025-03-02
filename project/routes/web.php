08cbb370a9155f4992d383f8c1dff7baa50d49e0
Fumiaki Nagashima <info@alcogy.com>
2025-03-02 22:07:24 +0900

    Add user controller.

--- project/routes/web.php
+++ project/routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/user/list', [UserController::class, 'list']);
+Route::get('/user/detail/{id}', [UserController::class, 'detail']);
+Route::get('/user/create', [UserController::class, 'register']);
+Route::post('/user/create', [UserController::class, 'create']);
+Route::put('/user/update', [UserController::class, 'update']);
+Route::delete('/user/delete', [UserController::class, 'delete']);
