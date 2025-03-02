f25baf14d205b14ffe894611ced8250956ab6109
Fumiaki Nagashima <info@alcogy.com>
2025-03-02 21:19:22 +0900

    Upgrade laravel.

--- project/routes/web.php
+++ project/routes/web.php
@@ -3,9 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
-});
-
-Route::get('/sample', function () {
-    return view('sample');
+    return view('welcome');
 });
